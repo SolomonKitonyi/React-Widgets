5f44482238fc0ed5f2e9f386db9db1984fa868b2
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-02-19 17:21:33 +0300

    add changes to the search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,24 @@
-import React,{useState}from 'react';
-
+import React,{useState,useEffect}from 'react';
+import axios from 'axios';
+import { queryAllByAltText } from '@testing-library/react';
 const Search = ()=>{
-    const [term, setTerm] = useState('');
+    const [term, setTerm] = useState('programming');
+    const [results,setResults] = useState([]);
+    useEffect(()=>{
+        const search = async()=>{
+           const {data}= await axios.get('https://en.wikipedia.org/w/api.php',{
+                params:{
+                    action: 'query',
+                    list: 'search',
+                    origin: '*',
+                    format: 'json',
+                    srsearch: term
+                }
+            });
+            setResults(data.query.search);
+        }
+        search();
+    })
     return (
         <div>
             <div className= "ui form">
