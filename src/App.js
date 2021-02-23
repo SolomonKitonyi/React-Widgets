e95566d437373743d829c979da2234b16b1d9c48
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-02-23 18:24:27 +0300

    add new component dropdown.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
+import Dropdown from './components/Dropdown';
+const options =[
+    {
+        label: 'The color of Red',
+        value: "red"
+    },
+    {
+        label: 'The color of Green',
+        value: 'green',
+    },
+    {
+        label: 'A shade of Blue',
+        value: 'blue'
+    }
+]
 const items = [
     {
         title: 'What is React',
@@ -16,9 +31,14 @@ const items = [
     }
 ];
 export default ()=>{
+    const [selected,setSelected] = useState(options[0])
     return (
         <div>
-           <Search/>
+           <Dropdown 
+           selected = {selected}
+           OnSelectedChange = {setSelected}
+           options={options}
+           />
         </div>
     );
 };
