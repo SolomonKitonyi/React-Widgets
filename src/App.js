b38b649b28af1dc7a80eb46e7a8ac5f1bec99829
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-02-24 20:54:12 +0300

    add new component Translate.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
 import Dropdown from './components/Dropdown';
+import Translate from './components/Translate';
 const options =[
     {
         label: 'The color of Red',
@@ -31,18 +32,11 @@ const items = [
     }
 ];
 export default ()=>{
-    const [selected,setSelected] = useState(options[0])
-    const [showDropdown,setShowDropdown]=useState(true);
+    //const [selected,setSelected] = useState(options[0])
+   // const [showDropdown,setShowDropdown]=useState(true);
     return (
         <div>
-            <button onClick = {()=>setShowDropdown(!showDropdown)}>Toggle Dropdown</button>
-          {showDropdown?
-            <Dropdown 
-           selected = {selected}
-           OnSelectedChange = {setSelected}
-           options={options}
-           />:null
-          }
+          <Translate/>
         </div>
     );
 };
